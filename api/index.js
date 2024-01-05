6c5f5d7ff625baebca1829129564702ee275a3f7
alex.abainza <abainza.alexandra@gmail.com>
2024-01-05 17:10:38 +0800

    create update user api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import cookieParser from 'cookie-parser';
 const app = express();
+app.use(cookieParser())
 app.use(express.json());
 dotenv.config();
 
