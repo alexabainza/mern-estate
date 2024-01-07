bc9edeaf24c545e7078fd5b2b6b767d71f7e2a8c
alex.abainza <abainza.alexandra@gmail.com>
2024-01-07 19:20:42 +0800

    finished edit lisitng route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,35 +1,55 @@
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createListing = async(req, res, next) => {
-    try{
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing)
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
-    }
-    catch(error){
-        next(error)
+export const deleteListing = async (req, res, next) => {
+  //check if listing exists
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  //check if the authenticated id is equal to the userRef
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings"));
+  }
 
-    }
-}
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteListing = async(req, res, next) => {
-    //check if listing exists
-    const listing = await Listing.findById(req.params.id)
-    if(!listing){
-        return next(errorHandler(404, "Listing not found"))
-    }
-    //check if the authenticated id is equal to the userRef
-    if(req.user.id !== listing.userRef){
-        return next(errorHandler(401, "You can only delete your own listings"))
-    }
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
 
-    try{
-        await Listing.findByIdAndDelete(req.params.id)
-        res.status(200).json("Listing has been deleted!");
-    }
-    catch(error){
-        next(error)
-    }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listing"));
+  }
 
-}
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      //to get the updated listing
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
