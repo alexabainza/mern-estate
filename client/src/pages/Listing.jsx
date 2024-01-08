0168e40b5f046e734d8b9d4c688f72a46de8f057
alex.abainza <abainza.alexandra@gmail.com>
2024-01-08 19:19:55 +0800

    add contact landlord functionality to the listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
+import { useSelector } from "react-redux"
 import "swiper/css/bundle";
 import {
   FaBath,
@@ -13,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from "react-icons/fa";
+import Contact from "../components/Contact";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -20,6 +22,9 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false)
+  const {currentUser} = useSelector((state) => state.user)
+  
 
   const params = useParams();
   useEffect(() => {
@@ -67,9 +72,9 @@ export default function Listing() {
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
             <FaShare
-              className='text-slate-500'
+              className="text-slate-500"
               onClick={() => {
                 navigator.clipboard.writeText(window.location.href);
                 setCopied(true);
@@ -80,7 +85,7 @@ export default function Listing() {
             />
           </div>
           {copied && (
-            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
               Link copied!
             </p>
           )}
@@ -100,32 +105,44 @@ export default function Listing() {
               <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
                 {listing.type === "rent" ? "For Rent" : "For Sale"}
               </p>
-              {
-                listing.offer && (
-                  <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">${+listing.regularPrice - +listing.discountPrice}</p>
-                )
-              }
+              {listing.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                  ${+listing.regularPrice - +listing.discountPrice}
+                </p>
+              )}
             </div>
-            <p className="text-slate-800"><span className="font-semibold text-black">Description - </span>{listing.description}</p>
-          <ul className=" text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
-            <li className="flex items-center gap-1 whitespace-nowrap">
-              <FaBed className="text-lg"/>
-              {listing.bedrooms > 1? `${listing.bedrooms} beds`:`${listing.bedrooms} bed` }
-            </li>
-            <li className="flex items-center gap-1 whitespace-nowrap">
-              <FaBath className="text-lg"/>
-              {listing.bathrooms > 1? `${listing.bathrooms} baths`:`${listing.bathrooms} bath` }
-            </li>
-            <li className="flex items-center gap-1 whitespace-nowrap">
-              <FaParking className="text-lg"/>
-              {listing.parking ? "Parking spot" :"No Parking" }
-            </li>
-            <li className="flex items-center gap-1 whitespace-nowrap">
-              <FaChair className="text-lg"/>
-              {listing.furnished? "Furnished": "Unfurnished" }
-            </li>
-          </ul>
-          
+            <p className="text-slate-800">
+              <span className="font-semibold text-black">Description - </span>
+              {listing.description}
+            </p>
+            <ul className=" text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaBed className="text-lg" />
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} beds`
+                  : `${listing.bedrooms} bed`}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaBath className="text-lg" />
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} baths`
+                  : `${listing.bathrooms} bath`}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaParking className="text-lg" />
+                {listing.parking ? "Parking spot" : "No Parking"}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap">
+                <FaChair className="text-lg" />
+                {listing.furnished ? "Furnished" : "Unfurnished"}
+              </li>
+            </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={()=> setContact(true)} className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3">
+                Contact Landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing}/>}
           </div>
         </>
       )}
