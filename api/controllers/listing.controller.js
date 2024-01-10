d29a90311dee73dc8d854ef41734e48df694afcf
alex.abainza <abainza.alexandra@gmail.com>
2024-01-10 16:15:31 +0800

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -54,15 +54,67 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export const getListing = async(req, res, next)=>{
-  try{
-    const listing = await Listing.findById(req.params.id)
-    if(!listing){
-      return next(errorHandler(404, "Listing not found!"))
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
     }
-    res.status(200).json(listing)
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
   }
-  catch(error){
-    next(error)
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    //if there is a limit, use it, parse it and get a number
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer; //can be true false or undefind
+
+    if (offer === undefined || offer === "false") {
+      //search in database both true and false
+      offer = { $in: [false, true] };
+    }
+    //if offer is selected, we want ony the offer
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      //search in database both true and false
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      //search in database both true and false
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    //all
+    if (type === undefined || parking === "all") {
+      //search in database both true and false
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      //i is for case insenstivity
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    }).sort({ [sort]: order })
+    .limit(limit).skip(startIndex) //sort
+
+    return res.status(200).json(listings)
+  } catch (error) {
+    next(error);
   }
-}
+};
