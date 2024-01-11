713da4ee398c47591351e8c93ee1158d96c24af2
Alex Abainza <abainza.alexandra@gmail.com>
2024-01-11 22:31:29 +0800

    added onChange and onSubmit ot searchpage

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -68,53 +68,104 @@ export const getListing = async (req, res, next) => {
 
 export const getListings = async (req, res, next) => {
   try {
-    //if there is a limit, use it, parse it and get a number
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
-    let offer = req.query.offer; //can be true false or undefind
+    let offer = req.query.offer;
 
-    if (offer === undefined || offer === "false") {
-      //search in database both true and false
+    if (offer === undefined || offer === 'false') {
       offer = { $in: [false, true] };
     }
-    //if offer is selected, we want ony the offer
+
     let furnished = req.query.furnished;
-    if (furnished === undefined || furnished === "false") {
-      //search in database both true and false
+
+    if (furnished === undefined || furnished === 'false') {
       furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
-    if (parking === undefined || parking === "false") {
-      //search in database both true and false
+
+    if (parking === undefined || parking === 'false') {
       parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
-    //all
-    if (type === undefined || parking === "all") {
-      //search in database both true and false
-      type = { $in: ["sale", "rent"] };
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
     }
 
-    const searchTerm = req.query.searchTerm || "";
+    const searchTerm = req.query.searchTerm || '';
 
-    const sort = req.query.sort || "createdAt";
+    const sort = req.query.sort || 'createdAt';
 
-    const order = req.query.order || "desc";
+    const order = req.query.order || 'desc';
 
     const listings = await Listing.find({
-      //i is for case insenstivity
-      name: { $regex: searchTerm, $options: "i" },
+      name: { $regex: searchTerm, $options: 'i' },
       offer,
       furnished,
       parking,
       type,
-    }).sort({ [sort]: order })
-    .limit(limit).skip(startIndex) //sort
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
 
-    return res.status(200).json(listings)
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
 };
+
+// export const getListings = async (req, res, next) => {
+//   try {
+//     //if there is a limit, use it, parse it and get a number
+//     const limit = parseInt(req.query.limit) || 9;
+//     const startIndex = parseInt(req.query.startIndex) || 0;
+//     let offer = req.query.offer; //can be true false or undefind
+
+//     if (offer === undefined || offer === "false") {
+//       //search in database both true and false
+//       offer = { $in: [false, true] };
+//     }
+//     //if offer is selected, we want ony the offer
+//     let furnished = req.query.furnished;
+//     if (furnished === undefined || furnished === "false") {
+//       //search in database both true and false
+//       furnished = { $in: [false, true] };
+//     }
+
+//     let parking = req.query.parking;
+//     if (parking === undefined || parking === "false") {
+//       //search in database both true and false
+//       parking = { $in: [false, true] };
+//     }
+
+//     let type = req.query.type;
+//     //all
+//     if (type === undefined || parking === "all") {
+//       //search in database both true and false
+//       type = { $in: ["sale", "rent"] };
+//     }
+
+//     const searchTerm = req.query.searchTerm || "";
+
+//     const sort = req.query.sort || "createdAt";
+
+//     const order = req.query.order || "desc";
+
+//     const listings = await Listing.find({
+//       //i is for case insenstivity
+//       name: { $regex: searchTerm, $options: "i" },
+//       offer,
+//       furnished,
+//       parking,
+//       type,
+//     }).sort({ [sort]: order })
+//     .limit(limit).skip(startIndex) //sort
+
+//     return res.status(200).json(listings)
+//   } catch (error) {
+//     next(error);
+//   }
+// };
